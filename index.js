10f9ba8023e580ebc16088fb13e0580d0b4c513c
Yerim Hu <yerimhu88@gmail.com>
2017-10-18 01:32:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var greeting = "Hello, everybody!";
+// the value of the greeting variable has to be "Hello, everybody!"
 
 var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+// we use concatenation to add string "Hello, " to make greetSpecialGuest variable value is "Hello, Neil deGrasse Tyson!"
 
 var topic = "space";
 
 var conversation = `Let's talk about ${topic}.`;
+// we use placeholders inside the back tick to join the strings. ${topic} is "space" 
